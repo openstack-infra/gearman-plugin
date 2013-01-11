c5fb8b5e42ec15d7e3086de8a354f6a82880aa78
Khai Do <do.khai@gmail.com>
2013-01-11 15:09:50 -0800

    Gearman configuration UI is setup in the jenkins config page
    Change-Id: I46a03e51b9d2eafdc71a6cdbff46992b82a7c820

--- src/main/java/hudson/plugins/gearman/GearmanPlugin.java
+++ src/main/java/hudson/plugins/gearman/GearmanPlugin.java
@@ -25,8 +25,12 @@ import hudson.model.BuildListener;
 import hudson.model.AbstractBuild;
 import hudson.tasks.Builder;
 import hudson.tasks.BuildStepDescriptor;
+
+import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.management.Descriptor;
 
@@ -44,10 +48,13 @@ import net.sf.json.JSONObject;
  */
 public class GearmanPlugin extends Builder {
 
+    private static final Logger logger = LoggerFactory
+            .getLogger(GearmanPlugin.class);
     private final String name;
 
     @DataBoundConstructor
     public GearmanPlugin(String name) {
+        logger.info("--- GearmanPlugin Constructor ---" + name);
         this.name = name;
     }
 
@@ -68,10 +75,29 @@ public class GearmanPlugin extends Builder {
     }
 
     @Extension
-    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+    public static final class DescriptorImpl extends
+            BuildStepDescriptor<Builder> {
         
+        private static final Logger logger = LoggerFactory
+                .getLogger(DescriptorImpl.class);
+        private boolean launchWorker; // launchWorker state (from UI checkbox)
+        private String host; // gearman server host
+        private int port; // gearman server port
+
         public DescriptorImpl() {
+            logger.info("--- DescriptorImpl Constructor ---");
+            logger.info("--- DescriptorImpl Constructor ---" + host);
+            logger.info("--- DescriptorImpl Constructor ---" + port);
+
             load();
+
+            /*
+             * Not sure when to register gearman functions yet so for now always
+             * initialize the launch worker flag to disabled state at jenkins
+             * startup so we are always at a known state
+             */
+            this.launchWorker = false;
+            save();
         }
 
         @Override
@@ -85,11 +111,65 @@ public class GearmanPlugin extends Builder {
         }
 
         @Override
-        public boolean configure(StaplerRequest staplerRequest, JSONObject json) throws FormException {
-            
+        public boolean configure(StaplerRequest staplerRequest, JSONObject json)
+                throws FormException {
+            launchWorker = json.getBoolean("launchWorker");
+            logger.info("--- DescriptorImpl Configure function ---"
+                    + this.launchWorker());
+
+            // set the gearman server host from value in jenkins config page
+            try {
+                host = json.getString("host");
+            } catch (Exception e) {
+                throw new RuntimeException(
+                        "Error getting the gearman host name");
+            }
+
+            // user input vaerification
+            if (StringUtils.isEmpty(host) || StringUtils.isBlank(host))
+                throw new RuntimeException("Invalid gearman host name");
+
+            // set the gearman server port from value in jenkins config page
+            try {
+                port = Integer.parseInt(json.getString("port"));
+            } catch (Exception e) {
+                throw new RuntimeException("Invalid gearman port value");
+            }
+
+            // i believe gearman already checks for port range, just want to do
+            // basic verification here
+            if (port <= 0)
+                throw new RuntimeException("Invalid gearman port value");
+
+            logger.info("--- DescriptorImpl Configure function ---"
+                    + this.getHost());
+            logger.info("--- DescriptorImpl Configure function ---"
+                    + this.getPort());
+
             save();
             return true;
         }
 
+        /**
+         * This method returns true if the global configuration says we should
+         * launch worker.
+         */
+        public boolean launchWorker() {
+            return launchWorker;
+        }
+
+        /**
+         * This method returns the value from the server host text box
+         */
+        public String getHost() {
+            return host;
+        }
+
+        /**
+         * This method returns the value from the server port text box
+         */
+        public int getPort() {
+            return port;
+        }
     }
 }
